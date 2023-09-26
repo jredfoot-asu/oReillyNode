926a5713a02c4db219d2fd4afd0fab5fb9881e78
Jason Redfoot <jredfoot@asu.edu>
2023-09-25 23:32:13 -0400

    adding chapter 3 coding work.

---
+++ Node/Ch3-The_Basics_Of_Node/create_Node_Server.js
@@ -0,0 +1,47 @@
+// Not using express
+const http = require('http');
+const fs = require('fs');
+
+// function reqListener(req, res) {
+
+// }
+
+const server = http.createServer((req, res) => {
+    // console.log(req.url, req.method, req.headers);
+    // will kill the server
+    // process.exit();
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk)
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server</h1></body>')
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);
